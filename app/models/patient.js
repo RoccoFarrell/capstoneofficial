f8d63d77cb76ccadad8d09fa01c86cd4c3acac1e
Josh Martin <josh.martin819@gmail.com>
2015-02-20 13:45:05 -0500

    Added welcome user. Added back buttons to login and new user. Other various tiny fixes.

--- app/models/patient.js
+++ app/models/patient.js
@@ -8,24 +8,10 @@ var patientSchema = new Schema({
 		street: {type: String, required: true},
 		city: {type: String, required: true},
 		state: {type: String, required: true},
-		zipcode: {type: Number, required: true}
+		zipcode: {type: String, required: true}
 	},
 	patientCaretaker: {type: String, required: true}
 	//Add room count object
 });
 
 module.exports = mongoose.model('Patient', patientSchema);
-
-/* For inserting manually into MongoDB shell
-({
-	patientName: "Josh Sukzacodin",
-	patientAge: 69,
-	patientAddress: [{
-		street: "1 Beatsauce Lane",
-		city: "Jackmehoff",
-		state: "NJ",
-		zipcode: 07750
-	}],
-	patientCaretaker: "capstone"
-})
-*/
